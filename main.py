e0153ca7ca9a00f84eacfd32ab2867cbe1291ff2
randriamanantenaOnenantsoaAngelie <onenantsoarandriamanantena@gmail.com>
2026-01-05 12:23:31 +0100

    Fix formatting of allow_credentials in CORS middleware

--- main.py
+++ main.py
@@ -48,11 +48,10 @@ origins = [
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
-    allow_credentials=True,
+    allow_credentials=True, 
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 # ======================================================
 # STARTUP (REMPLACE LIFESPAN)
 # ======================================================
