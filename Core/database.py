32f7524276df35d4ce66ab069e666fb9290d71d6
randriamanantenaOnenantsoaAngelie <onenantsoarandriamanantena@gmail.com>
2026-01-11 22:51:37 +0100

    Refactor database connection and session settings
    Updated database connection settings and improved session management.

--- Core/database.py
+++ Core/database.py
@@ -1,17 +1,30 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 from Core.config import settings
 
-SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
+DATABASE_URL = settings.DATABASE_URL
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+engine = create_engine(
+    DATABASE_URL,
+    pool_size=5,          # limite connexions Render
+    max_overflow=0,
+    pool_timeout=30,
+    pool_recycle=1800,
+    pool_pre_ping=True
+)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
 Base = declarative_base()
+
 
 def get_db():
     db = SessionLocal()
     try:
         yield db
     finally:
-        db.close()
+        db.close()  # 🔥 OBLIGATOIRE SUR RENDER
